f10167cb53710650d6a7a6e44fa5b97e7037f5a6
arun <allamarunkumar@gmail.com>
2025-09-02 17:23:25 +0530

    files added

--- templatedataproject/myapp/views.py
+++ templatedataproject/myapp/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from myapp.models import Employee
-# Create your views here.
 def getEmployees(request):
-    emp_list = Employee.objects.all();
+    emp_list=Employee.objects.all()
     my_dict = {'emp_list':emp_list}
     return render(request,'myapp/index.html',my_dict)
-
